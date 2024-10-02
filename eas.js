aad184743bd7bb779e5db746dc8abb31c03d9532
famzazn <famzazn@proton.me>
2024-10-02 02:06:47 -0400

    Add container and 256 divs

--- eas.js
+++ eas.js
@@ -1,1 +1,10 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+
+
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.textContent = "square";
+    container.appendChild(square);
+}
