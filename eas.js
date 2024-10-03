ee7535061144abfe2fe42f5fe38f7a43de922b5c
famzazn <famzazn@proton.me>
2024-10-03 00:00:06 -0400

    Create 16 x 16 grid.

--- eas.js
+++ eas.js
@@ -1,10 +1,18 @@
 const container = document.querySelector(".container");
 
 
+for (i = 1; i < 17; i++) {
+    const cont = document.createElement("div");
+    cont.classList.add(i);
+    container.appendChild(cont);
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.textContent = i;
-    container.appendChild(square);
+
+    for (let j = 1; j < 17; j++) {
+        const grid = document.createElement("div");
+        grid.classList.add(j);
+        grid.textContent = j;
+        cont.appendChild(grid);  
+    }
+    
 }
+
