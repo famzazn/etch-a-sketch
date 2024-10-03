d1815ee9a21e2679de2e80f87f016a9a3da43732
famzazn <famzazn@proton.me>
2024-10-02 23:32:41 -0400

    Add css flex styling to grid

--- eas.js
+++ eas.js
@@ -5,6 +5,6 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.textContent = "square";
+    square.textContent = i;
     container.appendChild(square);
 }
