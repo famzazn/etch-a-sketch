a61f9e1fbd6810469ccdf98f28d471f0fa5318f7
famzazn <famzazn@proton.me>
2024-10-03 00:14:57 -0400

    Clean up initial flex definitions

--- eas.js
+++ eas.js
@@ -3,14 +3,13 @@ const container = document.querySelector(".container");
 
 for (i = 1; i < 17; i++) {
     const cont = document.createElement("div");
-    cont.classList.add(i);
+    cont.classList.add("cont");
     container.appendChild(cont);
-
 
     for (let j = 1; j < 17; j++) {
         const grid = document.createElement("div");
-        grid.classList.add(j);
-        grid.textContent = j;
+        grid.classList.add(`${j}`);
+        grid.textContent = `${j}`;
         cont.appendChild(grid);  
     }
     
