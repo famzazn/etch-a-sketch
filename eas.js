40102c4bda8a76d57a2823e5b215d568124ba668
famzazn <famzazn@proton.me>
2024-10-04 21:14:22 -0400

    Add Custom Grid size option

--- eas.js
+++ eas.js
@@ -1,26 +1,45 @@
 const container = document.querySelector(".container");
 
+let a = 17
 
-for (i = 1; i < 17; i++) {
-    const cont = document.createElement("div");
-    cont.classList.add("cont");
-    container.appendChild(cont);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    a = prompt('Enter a number')
+    document.querySelectorAll('.container').forEach((element) => {
+        element.innerHTML = "";
+    }) 
+    grid();
+})
 
-    for (let j = 1; j < 17; j++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        cont.appendChild(grid);  
+
+function grid() {
+    for (i = 1; i < a; i++) {
+        const cont = document.createElement("div");
+        cont.classList.add("cont");
+        container.appendChild(cont);
+    
+        for (let j = 1; j < a; j++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            cont.appendChild(grid);  
+        }
+        
+    }
+    
+    
+    // querySelectorAll makes NodeList of all "grid" divs which acts like an array //
+    const gridcells = document.querySelectorAll('.grid'); 
+    
+    // Can now use forEach() to go through every single grid div to specify color input //
+    gridcells.forEach((cell) => cell.addEventListener('mouseover', color));
+    
+    function color(e) {
+        console.log('pass through cell');
+        e.target.style.backgroundColor = 'red'
     }
     
 }
 
-// querySelectorAll makes NodeList of all "grid" divs which acts like an array //
-const gridcells = document.querySelectorAll('.grid'); 
+grid();
 
-// Can now use forEach() to go through every single grid div to specify color input //
-gridcells.forEach((cell) => cell.addEventListener('mouseover', color));
 
-function color(e) {
-    console.log('pass through cell');
-    e.target.style.backgroundColor = 'red'
-}
